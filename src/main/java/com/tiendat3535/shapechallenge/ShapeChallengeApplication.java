a6763e81b48276472f0c4d2c5354da1585ffae11
Pham Tien Dat <phamtiendat@Macintosh-HD-HighSierra.local>
2019-10-12 14:03:00 +0700

    Project Initial

---
+++ src/main/java/com/tiendat3535/shapechallenge/ShapeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.tiendat3535.shapechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendat3535/shapechallenge/ShapeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendat3535.shapechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShapeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
